6a3516c9ed5d47c09e291350f593b46b9a5c3f7c
Jayden Pahukula <72293021+JaydenPahukula@users.noreply.github.com>
2022-10-07 09:56:31 -0600

    10.7.22

---
+++ 070 - Totient Permutation.py
@@ -0,0 +1,17 @@
+import math
+
+def phi(n:int):
+    bigList = [True for i in range(1, n+1)]
+    for i in range(2, n+1):
+        print(i)
+        for j in range(2*i, n+1, i):
+            print(" ", j)
+
+def solution(n:int):
+    
+
+    return
+
+print(phi(9))
+
+
